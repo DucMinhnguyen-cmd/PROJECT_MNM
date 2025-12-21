6adeb96414b1c0d244ed63475b7d8d16649413a2
DucMinhnguyen-cmd <nagato.minh@gmail.com>
2025-12-22 01:41:49 +0700

    up

--- du_an.py
+++ du_an.py
@@ -3,7 +3,6 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
 
 # 1. CẤU HÌNH SQLITE
 DB_FILE = r"D:\Program\PROJECT\cellphones.db"
